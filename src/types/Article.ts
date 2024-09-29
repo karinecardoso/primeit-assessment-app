0e47496358e2770d41634e8719f98cf58426e593
Karine Cardoso <karine.cardoso_@hotmail.com>
2024-09-29 22:21:16 +0100

    Add enums for Version, Currency and Measure

--- src/types/Article.ts
+++ src/types/Article.ts
@@ -1,26 +1,26 @@
 export interface Article {
-    id: number;
-    region: ArticleItem;
-    articleType: ArticleItem;
-    legalEntity: ArticleItem;
-    version: string;
-    currency: string;
-    measure: string;
-    value: number;
-  }
-  
-  export interface ArticleItem {
-    id: string;
-    name: string;
-  }
+  id: number;
+  region: ArticleItem;
+  articleType: ArticleItem;
+  legalEntity: ArticleItem;
+  version: Version;
+  currency: Currency;
+  measure: Measure;
+  value: number;
+}
 
-  export interface ArticleFilter {
-    regionId?: number;
-    articleTypeId?: number;
-    legalEntityId?: number;
-    version?: string;
-    currency?: string;
-    measure?: string;
+export interface ArticleItem {
+  id: string;
+  name: string;
+}
+
+export interface ArticleFilter {
+  regionId?: number;
+  articleTypeId?: number;
+  legalEntityId?: number;
+  version?: string;
+  currency?: string;
+  measure?: string;
 }
 
 export interface ArticleTableItem {
@@ -31,4 +31,21 @@ export interface ArticleTableItem {
   measure?: string;
   value?: number;
   subItems?: ArticleTableItem[];
+}
+
+export enum Version {
+  Actual = "Actual",
+  Budget = "Budget",
+}
+
+export enum Currency {
+  LC = "LC",
+  USD = "USD",
+  EUR = "EUR",
+}
+
+export enum Measure {
+  Units = "Units",
+  UnitPrice = "Unit Price",
+  GrossRevenue = "Gross Revenue",
 }
